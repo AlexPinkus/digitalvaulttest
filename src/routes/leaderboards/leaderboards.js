33b73687bb06b0a0956bab7acd6858ab063fd90c
José Alejandro Castillo Pinkus <Alexpinkus@hotmail.com>
2024-04-03 13:24:21 -0600

    Update leaderboards.js

--- src/routes/leaderboards/leaderboards.js
+++ src/routes/leaderboards/leaderboards.js
@@ -2,12 +2,19 @@ import express from 'express';
 import  {createLeaderboards,addUserToLeaderboard,updateUserScore,deleteLeaderboard,getLeaderboard,getUserRank,getUserNearestRank} from '../../controllers/leaderboards/leaderboards';
 
 const router = express.Router();
-router.post('/create', createLeaderboards);
-router.delete('/delete', deleteLeaderboard);
-router.patch('/addUser', addUserToLeaderboard);
-router.patch('/updatescore/:idLeaderboard/:idUser', updateUserScore);
+// Follo REST conventions, no need to add the verb.
+router.post('', createLeaderboards);
+router.delete('', deleteLeaderboard);
+
+// To add users I'd use a put, if the resource doesn't exists it creates it.
+router.put('/:idLeaderboard/users', addUserToLeaderboard);
+// When accessing a nested resource you need to use the correct format:
+router.patch('/:idLeaderboard/users/:idUser', updateUserScore);
 router.get('/middle/:idLeaderboard/:size/', getUserNearestRank);
-router.get('/:idLeaderboard/:idUser', getUserRank);
-router.get('/:idLeaderboard/:size/:page', getLeaderboard);
+
+// When accessing a nested resource you need to use the correct format:
+router.get('/:idLeaderboard/users/:idUser', getUserRank);
+// size and page should be handled as query params not path params
+router.get('/:idLeaderboard', getLeaderboard);
 
 export default router;
