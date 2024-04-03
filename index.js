ac09aa81663bb123178f7f262ef9eb27a4196344
José Alejandro Castillo Pinkus <Alexpinkus@hotmail.com>
2024-04-03 12:51:38 -0600

    Removed console.log

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ import connectDB from "./config/mongodb.js"
 import usersRoutes from "./src/routes/users/users.js"
 import learboardsRoutes from './src/routes/leaderboards/leaderboards.js';
 
-//const {onRequest} = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
-//const functions = require('firebase-functions');
 dotenv.config()
 connectDB()
 
