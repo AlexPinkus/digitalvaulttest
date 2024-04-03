9b121b21499d656ebd1d173fb30e8737fd5ef416
José Alejandro Castillo Pinkus <Alexpinkus@hotmail.com>
2024-04-03 13:20:16 -0600

    Update scoresUnused.js

--- src/models/scoresUnused.js
+++ src/models/scoresUnused.js
@@ -1,16 +1,1 @@
-import mongoose from "mongoose";
-
-const usersSchema =  new mongoose.Schema({
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    score: {
-        type: Number,
-        default:0,
-    }
-
-});
-
-const users = mongoose.model('users', usersSchema);
-export default users;
+// Remove this file
